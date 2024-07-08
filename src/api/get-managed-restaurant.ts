18af7a8fb13e63f4f4b6f5d40241cc4c9b201fbb
= <=>
2024-07-08 00:42:39 -0300

    docs: add comment about get-managed-restaurant api

---
+++ src/api/get-managed-restaurant.ts
@@ -0,0 +1,23 @@
+/* 08/07/2024 - Igor Castilhos
+* This code defines an async function that fetches
+* data about a managed restaurant using a GET req.
+* It expects the response to conform to the
+* GetManagedRestaurantsResponse interface,
+* which includes properties like name, id and
+* createdAt, and returns the data from the response.
+*/
+import {api} from "@/lib/axios.ts";
+
+export interface GetManagedRestaurantsResponse {
+    name: string
+    id: string
+    createdAt: Date | null
+    updatedAt: Date | null
+    description: string | null
+    managerId: string | null
+}
+
+export async function getManagedRestaurant() {
+    const response = await api.get<GetManagedRestaurantsResponse>('/managed-restaurant')
+    return response.data
+}
